565d6f7c8a3c919ae75f14b43d5cc340d74c74d3
Peter Murphy <petermurphy5672@gmail.com>
2024-10-18 18:36:10 +0100

    Implement POST route

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import readEmployeeData from '../services/csvService.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 let employees = [];
@@ -14,6 +15,17 @@ readEmployeeData()
 
 router.get('/', (req, res) => {
     res.send(employees);
+})
+
+router.post('/', (req, res) => {
+    // Take employee data from body of POST request
+    const employee = req.body;
+
+    // Add employee with uniquely generated id to employees array
+    employees.push({ ...employee, id: uuidv4() });
+    
+    // Send response back to client
+    res.send(`${employee.name} has been added to the Array.`);
 })
 
 export default router
